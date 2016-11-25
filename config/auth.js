c1dbc7b0cc47c44c37ffcd32f8b496c2b9a8e3cd
oskaryil <15bayi@stockholmscience.se>
2016-11-25 13:43:18 +0100

    updated auth.js

--- config/auth.js
+++ config/auth.js
@@ -2,16 +2,16 @@ module.exports = {
   'facebookAuth': {
     'clientID': '699460823553912',
     'clientSecret': '4f0b83cc094b3b0ac55f23ec64b1016b',
-    'callbackURL': 'http://localhost:8080/auth/facebook/callback'
+    'callbackURL': 'https://arduinoprojects.io/auth/facebook/callback'
   },
   'googleAuth': {
     'clientID': '176568116976-jcsannjcqspcej78eu1j8gqvod2s251f.apps.googleusercontent.com',
     'clientSecret': 'oOBNFT8jwPjYLm7QUqq-gnzG',
-    'callbackURL': 'http://localhost:8080/auth/google/callback'
+    'callbackURL': 'https://arduinoprojects.io/auth/google/callback'
   },
   'githubAuth': {
     'clientID': '274c7a9772f033c78023',
     'clientSecret': '9614a81ca07567c0a3a865afcbc730ca5dbf58bd',
-    'callbackURL': 'http://localhost:8080/auth/github/callback'
+    'callbackURL': 'https://arduinoprojects.io/auth/github/callback'
   }
 }
